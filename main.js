2f0fb34f05a6975e980843f8056b7cd4730723d4
adam1920-code <75556463+adam1920-code@users.noreply.github.com>
2021-01-25 14:21:30 +0700

    Update main.js

--- main.js
+++ main.js
@@ -194,7 +194,7 @@ conn.handler = async function (m) {
 
 conn.on('message-new', conn.handler) 
 conn.on('error', conn.logger.error)
-global.mods = ['6281357302007@s.whatsapp.net']
+global.mods = ['6283153843600@s.whatsapp.net']
 global.prems = ['62888235435804@s.whatsapp.net']
 
 global.dfail = (type, m, conn) => {
